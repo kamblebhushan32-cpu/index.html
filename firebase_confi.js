7b9f29c2fa1547f119824ff494d6f4c8936b2ccc
kamblebhushan32-cpu <kamblebhushan32@gmail.com>
2026-09-23 13:31:04 +0530

    Add files via upload

---
+++ firebase_confi.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB_N-Y7hQC1qMXlukQ-giTQWtAgOxNzdIQ",
+  authDomain: "maitri-aap.firebaseapp.com",
+  projectId: "maitri-aap",
+  storageBucket: "maitri-aap.firebasestorage.app",
+  messagingSenderId: "27530672643",
+  appId: "1:27530672643:web:ac2cef6483dcf0695eec36",
+  measurementId: "G-CE8XVNW065"
+};
